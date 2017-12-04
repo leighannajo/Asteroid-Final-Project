c9943b490ca1d79a24edb085a5a1d4dcb3f3c9b7
lillib123 <lblair2@emich.edu>
2017-12-04 13:50:34 -0500

    twitter feed working

--- asteroid-service.js
+++ asteroid-service.js
@@ -3,6 +3,7 @@
     var asteroidData;
     var arrayLength;
     var asteroidArray = [];
+    var moreAsteriod = [];
     return {
       makeRequest: makeRequest,
       getData: getData
@@ -15,6 +16,16 @@
         asteroidData = response;
         var arrayLength = asteroidData.data.near_earth_objects.length;
         var randomNumber = Math.floor(Math.random() * arrayLength);
+        asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
+        if (asteroidData.data.near_earth_objects[randomNumber].is_potentially_hazardous_asteroid === true){
+          asteroidArray.push("Is potentially hazardous? Yes");
+        } else if (asteroidData.data.near_earth_objects[randomNumber].is_potentially_hazardous_asteroid === false){
+          asteroidArray.push("Is potentially hazardous? No");
+        }
+
+asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
+asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
+asteroidArray.push("Average estimated diameter: "+((asteroidData.data.near_earth_objects[randomNumber].estimated_diameter.kilometers.estimated_diameter_min)+(asteroidData.data.near_earth_objects[randomNumber].estimated_diameter.kilometers.estimated_diameter_max))/2);
 
       for(var i=0; i< asteroidData.data.near_earth_objects[randomNumber].close_approach_data.length; i++) {
         asteroidArray.push("Close Approach Date" + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].close_approach_date);
@@ -25,6 +36,8 @@
         asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.kilometers + " kilometers");
         asteroidArray.push("Orbiting Body: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].orbiting_body);
       }
+
+        console.log(asteroidData.data.near_earth_objects[randomNumber]);
         //display random asteroid data ^^^
 
       });
--- home-component.js
+++ home-component.js
@@ -5,6 +5,8 @@ var homeComponent = {
   <button>this does nothing</button>
   <h1>{{$ctrl.today}}</h1>
   <p ng-repeat="info in $ctrl.asteroidArray track by $index">{{info}}</p>
+
+
   `
 };
 angular

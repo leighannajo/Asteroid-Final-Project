64f0f62a277387cbaea64e200f596047e28336d6
lillib123 <lblair2@emich.edu>
2017-12-04 11:09:18 -0500

    up to date copy

--- Asteroid-Final-Project-master/home-component.js
+++
@@ -1,9 +0,0 @@
-(function(){
-var homeComponent = {
-  controller: "Controller",
-  template: `<button ng-click="$ctrl.getData();">Click Here</button>`
-};
-angular
-.module("app")
-.component("homeComponent", homeComponent);
-})();

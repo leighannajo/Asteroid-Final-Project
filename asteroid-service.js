83bfe62ff4a5865eeff3fdc2c5c556f22c26fe1e
lillib123 <lblair2@emich.edu>
2017-12-04 13:55:38 -0500

    fixed css

--- asteroid-service.js
+++ asteroid-service.js
@@ -23,19 +23,19 @@
           asteroidArray.push("Is potentially hazardous? No");
         }
 
-asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
-asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
-asteroidArray.push("Average estimated diameter: "+((asteroidData.data.near_earth_objects[randomNumber].estimated_diameter.kilometers.estimated_diameter_min)+(asteroidData.data.near_earth_objects[randomNumber].estimated_diameter.kilometers.estimated_diameter_max))/2);
+        asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
+        asteroidArray.push("Name: " + asteroidData.data.near_earth_objects[randomNumber].name);
+        asteroidArray.push("Average estimated diameter: "+((asteroidData.data.near_earth_objects[randomNumber].estimated_diameter.kilometers.estimated_diameter_min)+(asteroidData.data.near_earth_objects[randomNumber].estimated_diameter.kilometers.estimated_diameter_max))/2);
 
-      for(var i=0; i< asteroidData.data.near_earth_objects[randomNumber].close_approach_data.length; i++) {
-        asteroidArray.push("Close Approach Date" + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].close_approach_date);
-        asteroidArray.push("Relative Velocity: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].relative_velocity.kilometers_per_hour + "kph");
-        asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].relative_velocity.miles_per_hour + "mph");
-        asteroidArray.push("Miss Distance: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.astronomical + "AU");
-        asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.miles + " miles");
-        asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.kilometers + " kilometers");
-        asteroidArray.push("Orbiting Body: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].orbiting_body);
-      }
+        for(var i=0; i< asteroidData.data.near_earth_objects[randomNumber].close_approach_data.length; i++) {
+          asteroidArray.push("Close Approach Date" + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].close_approach_date);
+          asteroidArray.push("Relative Velocity: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].relative_velocity.kilometers_per_hour + "kph");
+          asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].relative_velocity.miles_per_hour + "mph");
+          asteroidArray.push("Miss Distance: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.astronomical + "AU");
+          asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.miles + " miles");
+          asteroidArray.push(asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].miss_distance.kilometers + " kilometers");
+          asteroidArray.push("Orbiting Body: " + asteroidData.data.near_earth_objects[randomNumber].close_approach_data[i].orbiting_body);
+        }
 
         console.log(asteroidData.data.near_earth_objects[randomNumber]);
         //display random asteroid data ^^^

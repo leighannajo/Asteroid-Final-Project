6d8174789cb24f098e9a837f227b6e8567f35c2f
Leighanna Hooper <30450495+leighannajo@users.noreply.github.com>
2017-12-08 13:35:21 -0500

    link color change

--- home-component.js
+++ home-component.js
@@ -4,7 +4,7 @@
     <!-- <button>this does nothing</button> -->
     <div class="oftheday">
     <h1>Asteroid of the Day</h1>
-    <p ng-repeat="data in $ctrl.topAsteroidArray track by $index">{{data.name}}<br>{{data.potential}}<br>{{data.diameter}}<br> <a href="{{data.url}}">More info!</a></p>
+    <p ng-repeat="data in $ctrl.topAsteroidArray track by $index">{{data.name}}<br>{{data.potential}}<br>{{data.diameter}}<br> <a class="asteroidLink" href="{{data.url}}">More info!</a></p>
     <p ng-repeat="info in $ctrl.asteroidArray track by $index">{{info.date}}<br>{{info.kph}}<br>{{info.missau}}<br>{{info.missm}}<br>{{info.misskm}}<br>{{info.orbits}}</p>
     </div>
     `,

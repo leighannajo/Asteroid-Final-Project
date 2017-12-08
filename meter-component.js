24908953450441f09affa31cc8f2ecdaabad761f
lillib123 <lblair2@emich.edu>
2017-12-08 09:27:32 -0500

    meter functionality done

--- meter-component.js
+++ meter-component.js
@@ -2,18 +2,20 @@
 
   var meterComponent = {
     template: `
+    <div id="meter_container2">
     <h1>The End is Near</h1>
     <div meter-directive id="meter_container">
-    <div id="one" class="risk"></div>
-    <div id="two" class="risk"></div>
-    <div id="three" class="risk"></div>
-    <div id="four" class="risk"></div>
-    <div id="five" class="risk"></div>
-    <div id="six" class="risk"></div>
-    <div id="seven" class="risk"></div>
-    <div id="eight" class="risk"></div>
-    <div id="nine" class="risk"></div>
-    <div id="ten" class="risk"></div>
+    <div ng-if="$ctrl.display1" id="one" class="risk"></div>
+    <div ng-if="$ctrl.display2" id="two" class="risk"></div>
+    <div ng-if="$ctrl.display3" id="three" class="risk"></div>
+    <div ng-if="$ctrl.display4" id="four" class="risk"></div>
+    <div ng-if="$ctrl.display5" id="five" class="risk"></div>
+    <div ng-if="$ctrl.display6" id="six" class="risk"></div>
+    <div ng-if="$ctrl.display7" id="seven" class="risk"></div>
+    <div ng-if="$ctrl.display8" id="eight" class="risk"></div>
+    <div ng-if="$ctrl.display9" id="nine" class="risk"></div>
+    <div ng-if="$ctrl.display10" id="ten" class="risk"></div>
+    </div>
     </div>
     `,
     controller:
@@ -25,12 +27,88 @@
       vm.approachTime;
       vm.objectName;
       vm.risk;
+      vm.display1 = false;
+      vm.display2 = false;
+      vm.display3 = false;
+      vm.display4 = false;
+      vm.display5 = false;
+      vm.display6 = false;
+      vm.display7 = false;
+      vm.display8 = false;
+      vm.display9 = false;
+      vm.display10 = false;
 
 
       MeterService.makeRequest().then(function(risk) {
-
         console.log(risk);
-        vm.risk = risk;
+
+        if (risk === 1) {
+          vm.display1 = true;
+        } else if (risk === 2) {
+          vm.display1 = true;
+          vm.display2 = true;
+        } else if (risk === 3) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+        } else if (risk === 4) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+        } else if (risk === 5) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+          vm.display5 = true;
+        } else if (risk === 6) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+          vm.display5 = true;
+          vm.display6 = true;
+        } else if (risk === 7) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+          vm.display5 = true;
+          vm.display6 = true;
+          vm.display7 = true;
+        } else if (risk === 8) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+          vm.display5 = true;
+          vm.display6 = true;
+          vm.display7 = true;
+          vm.display8 = true;
+        } else if (risk === 9) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+          vm.display5 = true;
+          vm.display6 = true;
+          vm.display7 = true;
+          vm.display8 = true;
+          vm.display9 = true;
+        } else if (risk === 10) {
+          vm.display1 = true;
+          vm.display2 = true;
+          vm.display3 = true;
+          vm.display4 = true;
+          vm.display5 = true;
+          vm.display6 = true;
+          vm.display7 = true;
+          vm.display8 = true;
+          vm.display9 = true;
+          vm.display10 = true;
+        }
+
       });
     }
   }

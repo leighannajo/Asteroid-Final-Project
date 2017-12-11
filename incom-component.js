208df129bee0de85e58b3e006759d27c9e2256ab
lillib123 <lblair2@emich.edu>
2017-12-11 14:35:20 -0500

    some cool animations

--- incom-component.js
+++ incom-component.js
@@ -4,7 +4,9 @@
 
     <div class="incom">
     <h3>Incoming Asteroids!</h3>
-    <div class="container"><div class="asteroid" ng-repeat="objects in $ctrl.displayArray track by $index">{{objects.name}}<br>{{objects.date}}<br>{{objects.distanceau}}<br>{{objects.distancemi}}<br>{{objects.distancekm}}<br>{{objects.speed}}<br>{{objects.orbits}}</div>
+    <div class="container">
+    <div incom-directive class="asteroid" ng-repeat="objects in $ctrl.displayArray track by $index">{{objects.name}}<br>{{objects.date}}<br>{{objects.distanceau}}<br>{{objects.distancemi}}<br>{{objects.distancekm}}<br>{{objects.speed}}<br>{{objects.orbits}}
+    </div>
     </div>
     `,
     controller:
@@ -14,6 +16,7 @@
       vm.incomData;
       vm.displayArray = [];
       vm.asteroidObj;
+
       IncomService.startIncom().then(function(){
         vm.incomData = IncomService.getData();
 
--- module.js
+++ module.js
@@ -1,5 +1,5 @@
 (function(){
 
   angular
-    .module("app", ["ngRoute", "720kb.datepicker"])
+    .module("app", ["ngRoute", "720kb.datepicker", "ngAnimate"])
 })();

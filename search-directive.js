bac5fef2641c42e77a34ce958110b6d0b2d80aa7
lillib123 <lblair2@emich.edu>
2017-12-14 14:06:08 -0500

    added typewriter and calendar

--- search-directive.js
+++ search-directive.js
@@ -6,6 +6,7 @@
          $element.on('click', function(){
            var result2 = document.getElementsByClassName("_720kb-datepicker-calendar")[0];
            result2.classList.toggle("showing");
+           console.log("this is working");
          })
         }
       }
